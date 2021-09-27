680114508db269e4f70be70f6b1aa01670ccabba
lucasmarques2020 <luquinhas.mb07@gmail.com>
2021-09-27 10:21:33 -0300

    new application

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,8 @@
 	let armazenar = [];
 	let dados = {
 		id: "",
-		usuario: "",
-		senha: "",
-		email: "",
+		atividade: "",
+		data: "",
 		estado: false,
 	};
 	if (localStorage.getItem("armazenar")) {
@@ -14,15 +13,10 @@
 	$: localStorage.setItem("armazenar", JSON.stringify(armazenar));
 
 	const addDados = () => {
-		if (
-			!dados.usuario.trim() &&
-			!dados.senha.trim() &&
-			!dados.email.trim()
-		) {
+		if (!dados.atividade.trim() && !dados.data.trim()) {
 			alert("Campo vazio! Por favor preencha o campo.");
-			dados.usuario = "";
-			dados.senha = "";
-			dados.email = "";
+			dados.atividade = "";
+			dados.data = "";
 			return;
 		}
 		console.log(armazenar);
@@ -31,9 +25,8 @@
 		console.log(armazenar);
 		dados = {
 			id: "",
-			usuario: "",
-			senha: "",
-			email: "",
+			atividade: "",
+			data: "",
 			estado: false,
 		};
 	};
@@ -51,85 +44,91 @@
 </script>
 
 <main>
-	<h1>CRUD</h1>
-	<div class = "col-6">
-		<form
-		class="form-control shadow border-0 position-absolute top-50 end-0 translate-middle"
-	>	<h2>Cadastrar</h2>
-		<div class="mb-3">
-			<label for="exampleInputEmail1" class="form-label">Usuário</label>
-			<input
-				type="text"
-				class="form-control"
-				placeholder="Usuário"
-				bind:value={dados.usuario}
-			/>
-		</div>
-		<div class="mb-3">
-			<label for="exampleInputEmail1" class="form-label">Email</label>
-			<input
-				type="email"
-				class="form-control"
-				placeholder="email"
-				bind:value={dados.email}
-			/>
-		</div>
-		<div class="mb-3">
-			<label for="exampleInputPassword1" class="form-label">Senha</label>
-			<input
-				type="password"
-				class="form-control"
-				placeholder="senha"
-				bind:value={dados.senha}
-			/>
-		</div>
-		<button class="btn btn-primary" on:click|preventDefault={addDados}
-			>Cadastrar</button
-		>
-	</form>
-	</div>
-	
-	{#each armazenar as item}
-	
-		<div class= "col-6">
-			<h2>Contas criadas</h2>
-			<div class="shadow my-3 p-3 lead">
-				<p class={item.estado ? "text-decoration-line-through" : ""}>
-					Usuário: {item.usuario}
-				</p>
-				<p class={item.estado ? "text-decoration-line-through" : ""}>
-					Email: {item.email}
-				</p>
-				<p class={item.estado ? "text-decoration-line-through" : ""}>
-					Senha: {item.senha}
-				</p>
-				<button
-					class="btn btn-sm {classEstado(item.estado)}"
-					on:click={editarDados(item.id)}
-				>
-					<i class={classIcono(item.estado)} />
-				</button>
-				<button
-					class="btn btn-sm btn-danger"
-					on:click={deletarDados(item.id)}
-				>
-					<i class="bi bi-trash" />
-				</button>
+	<h1>CRUD ATIVIDADES</h1>
+	<div class="container">
+		<div class="row">
+			<div class="pai-form col-6">
+				<form class="form-control shadow border-0">
+					<h2>Cadastrar atividade</h2>
+					<div class="mb-3">
+						<label for="exampleInputdata1" class="form-label"
+							>Atividade</label
+						>
+						<input
+							type="text"
+							class="form-control"
+							placeholder="Atividade"
+							bind:value={dados.atividade}
+						/>
+					</div>
+					<div class="mb-3">
+						<label for="exampleInputdata1" class="form-label"
+							>Tempo</label
+						>
+						<input
+							type="date"
+							class="form-control"
+							placeholder="data"
+							bind:value={dados.data}
+						/>
+					</div>
+					<button
+						class="btn btn-primary"
+						on:click|preventDefault={addDados}>Cadastrar</button
+					>
+				</form>
 			</div>
+			{#each armazenar as item}
+				<div class="atividades col-6">
+					<h2>Atividades criadas</h2>
+					<div class="shadow my-3 p-3 lead">
+						<p
+							class={item.estado
+								? "text-decoration-line-through"
+								: ""}
+						>
+							Usuário: {item.atividade}
+						</p>
+						<p
+							class={item.estado
+								? "text-decoration-line-through"
+								: ""}
+						>
+							Data Limite: {item.data}
+						</p>
+						<button
+							class="btn btn-sm {classEstado(item.estado)}"
+							on:click={editarDados(item.id)}
+						>
+							<i class={classIcono(item.estado)} />
+						</button>
+						<button
+							class="btn btn-sm btn-danger"
+							on:click={deletarDados(item.id)}
+						>
+							<i class="bi bi-trash" />
+						</button>
+					</div>
+				</div>
+			{/each}
 		</div>
-	{/each}
+	</div>
 </main>
 
 <style>
 	main {
+		height: 100vh;
+		width: 100%;
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	}
+	.pai-form {
+		display: flex;
+		justify-content: center;
 	}
 	form {
-		width: 400px;
-		height: 500px;
+		max-width: 400px;
+		max-height: 500px;
 		border-radius: 20px;
 	}
 	h1 {
